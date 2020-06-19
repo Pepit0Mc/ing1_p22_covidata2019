a9035e31e338583b61a050a5159d52c55373058f
Userleef <wassim.ajili@epita.fr>
2020-06-19 15:14:49 +0200

    [Global Data] - create the view for the global Data

---
+++ app/src/main/java/com/example/covidata/GlobalData.kt
@@ -0,0 +1,12 @@
+package com.example.covidata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GlobalData : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_global_data)
+    }
+}
--- app/src/main/java/com/example/covidata/MainActivity.kt
+++ app/src/main/java/com/example/covidata/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.covidata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataBtn = findViewById<Button>(R.id.dataBtn)
+        dataBtn.setOnClickListener(View.OnClickListener() {
+            val intent = Intent(this, GlobalData::class.java)
+            startActivity(intent)
+        })
     }
 }
