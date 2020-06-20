00f293c3d6ca5d0c0ac491dafc3684e85162d923
Unknown <philippe.qiao@epita.fr>
2020-06-20 18:11:15 +0200

    [ERROR] Some error management

--- app/src/main/java/com/example/covidata/CountryData.kt
+++ app/src/main/java/com/example/covidata/CountryData.kt
@@ -1,9 +1,11 @@
 package com.example.covidata
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.CalendarView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_country_data.*
@@ -31,18 +33,21 @@ class CountryData : AppCompatActivity() {
 
         val callbackCountry: Callback<List<CountryStat>> = object : Callback<List<CountryStat>> {
             override fun onFailure(call: Call<List<CountryStat>>, t: Throwable) {
-                TODO("Not yet implemented")
+                Toast.makeText(applicationContext,"Couldn't access the API, check your internet connection", Toast.LENGTH_SHORT).show()
             }
 
             override fun onResponse(call: Call<List<CountryStat>>, response: Response<List<CountryStat>>) {
                 if (response.code() == 200 && response.body() != null) {
                     val countryData: List<CountryStat> = response.body() as List<CountryStat>
-                    val dateData: CountryStat = countryData[0]
-                    countryTextView.text = dateData.Country
-                    if (countryData.size > 1)
+
+                    if (countryData.size != 1)
                         StatCountryTextView.text = "No data available"
-                    else
-                        StatCountryTextView.text = "Confirmed: ${dateData.Confirmed}\nDeaths: ${dateData.Deaths}\nRecovered: ${dateData.Recovered}"
+                    else {
+                        val dateData: CountryStat = countryData[0]
+                        countryTextView.text = dateData.Country
+                        StatCountryTextView.text =
+                            "Confirmed: ${dateData.Confirmed}\nDeaths: ${dateData.Deaths}\nRecovered: ${dateData.Recovered}"
+                    }
                 }
             }
         }
